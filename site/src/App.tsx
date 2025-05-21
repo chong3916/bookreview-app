7f26d707f1cf165dc03b0447cbbacb40eab397d2
Cherie Hong <cherieho@usc.edu>
2025-05-20 22:02:15 -0700

    Added login, logout, and modified user model to have first and last name instead of username

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -2,15 +2,19 @@ import './App.css'
 import {Routes, Route, BrowserRouter} from "react-router";
 import HomePage from "./pages/HomePage.tsx";
 import SignupPage from "./pages/SignupPage.tsx";
+import LoginPage from "@/pages/LoginPage.tsx";
+import {AuthContextProvider} from "@/contexts/AuthContext.tsx";
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/signup" element={<SignupPage/>}/>
-                {/*<Route path="/login" element={<LoginPage/>}/>*/}
-                <Route path="/" element={<HomePage/>}/>
-            </Routes>
+            <AuthContextProvider>
+                <Routes>
+                    <Route path="/signup" element={<SignupPage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/" element={<HomePage/>}/>
+                </Routes>
+            </AuthContextProvider>
         </BrowserRouter>
     )
 }
