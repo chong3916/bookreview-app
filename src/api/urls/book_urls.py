18f38115423606498c68c129597cebbae7717963
Cherie Hong <cherieho@usc.edu>
2025-05-25 14:53:26 -0700

    Separated book_views for better maintainability, reusability, and readability

--- src/api/urls/book_urls.py
+++ src/api/urls/book_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from api.views.book_views import BookTermSearchView, BookDetailView, BookHardcoverSearchView
+from api.views.book_views import BookDetailView, BookHardcoverSearchView, TrendingView
 
 urlpatterns = [
     path('search', BookHardcoverSearchView.as_view(), name='book-term-search'),
-    path("<str:book_id>", BookDetailView.as_view(), name="book-detail")
+    path('<str:book_id>', BookDetailView.as_view(), name='book-detail'),
+    path('trending/<str:duration>', TrendingView.as_view(), name='books-trending')
 ]
