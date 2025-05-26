f262feb7197f660fc1edc5d8d3f6835198f519fc
Cherie Hong <cherieho@usc.edu>
2025-05-25 18:31:47 -0700

    Updated login and signup page

--- site/src/pages/LoginPage.tsx
+++ site/src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import {auth} from "../api/auth.ts";
 import {
     Card,
-    CardContent,
+    CardContent, CardDescription,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
@@ -10,8 +10,10 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import {useAuthContext} from "@/contexts/AuthContext.tsx";
+import {cn} from "@/lib/utils.ts";
+import {Link} from "react-router";
 
-const SignupPage: React.FC<{}> = () => {
+const LoginPage: React.FC<{}> = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const { authData, setAuthData } = useAuthContext();
@@ -38,33 +40,58 @@ const SignupPage: React.FC<{}> = () => {
     }
 
     return (
-        <div className="min-h-screen bg-gray-50 dark:bg-gray-600 content-center">
-            <Card className="max-w-sm m-auto">
-                <CardHeader>
-                    <CardTitle>Login</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <form onSubmit={handleSubmit} className="flex max-w-md flex-col gap-4">
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="email" type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} required />
-                            </div>
-                        </div>
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="password">Password</Label>
-                                <Input id="password" type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} required />
-                            </div>
-                        </div>
-
-
-                        <Button color="light" className="cursor-pointer bg-gray-200 dark:bg-gray-600 text-black dark:text-white" type="submit">Login</Button>
-                    </form>
-                </CardContent>
-            </Card>
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <div className={cn("flex flex-col gap-6")}>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle className="text-2xl">Login</CardTitle>
+                            <CardDescription>
+                                Enter your email below to login to your account
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <form onSubmit={handleSubmit}>
+                                <div className="flex flex-col gap-6">
+                                    <div className="grid gap-2">
+                                        <Label htmlFor="email">Email</Label>
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="grid gap-2">
+                                        <div className="flex items-center">
+                                            <Label htmlFor="password">Password</Label>
+                                        </div>
+                                        <Input id="password" type="password"
+                                               value={password}
+                                               onChange={(e) => setPassword(e.target.value)}
+                                               required />
+                                    </div>
+                                    <Button type="submit" className="w-full">
+                                        Login
+                                    </Button>
+                                    {/*<Button variant="outline" className="w-full">*/}
+                                    {/*    Login with Google*/}
+                                    {/*</Button>*/}
+                                </div>
+                                <div className="mt-4 text-center text-sm">
+                                    Don&apos;t have an account?{" "}
+                                    <Link to="/signup" className="underline underline-offset-4">
+                                        Sign up
+                                    </Link>
+                                </div>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 }
 
-export default SignupPage;
+export default LoginPage;
--- site/src/pages/SignupPage.tsx
+++ site/src/pages/SignupPage.tsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import {auth} from "../api/auth.ts";
 import {
     Card,
-    CardContent,
+    CardContent, CardDescription,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
+import {cn} from "@/lib/utils.ts";
+import {Link} from "react-router";
 
 const SignupPage: React.FC<{}> = () => {
     const [firstName, setFirstName] = useState<string>("");
@@ -33,43 +35,75 @@ const SignupPage: React.FC<{}> = () => {
     }
 
     return (
-        <div className="min-h-screen bg-gray-50 dark:bg-gray-600 content-center">
-            <Card className="max-w-sm m-auto">
-                <CardHeader>
-                    <CardTitle>Sign up</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <form onSubmit={handleSubmit} className="flex max-w-md flex-col gap-4">
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="email" type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} required />
-                            </div>
-                        </div>
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="firstName">First Name</Label>
-                                <Input id="firstName" type="text" value={firstName} onChange={(e) => { setFirstName(e.target.value) }} required />
-                            </div>
-                        </div>
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="lastName">Last Name</Label>
-                                <Input id="lastName" type="text" value={lastName} onChange={(e) => { setLastName(e.target.value) }} />
-                            </div>
-                        </div>
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="password">Password</Label>
-                                <Input id="password" type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} required />
-                            </div>
-                        </div>
-
-
-                        <Button color="light" className="cursor-pointer bg-gray-200 dark:bg-gray-600 text-black dark:text-white" type="submit">Sign up</Button>
-                    </form>
-                </CardContent>
-            </Card>
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <div className={cn("flex flex-col gap-6")}>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle className="text-2xl">Sign up</CardTitle>
+                            <CardDescription>
+                                Enter your info below to create an account
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <form onSubmit={handleSubmit}>
+                                <div className="flex flex-col gap-6">
+                                    <div className="grid gap-2">
+                                        <Label htmlFor="email">Email</Label>
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="grid gap-2">
+                                        <Label htmlFor="firstName">First Name</Label>
+                                        <Input
+                                            id="firstName"
+                                            type="text"
+                                            value={firstName}
+                                            onChange={(e) => setFirstName(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="grid gap-2">
+                                        <Label htmlFor="lastName">Last Name</Label>
+                                        <Input
+                                            id="lastName"
+                                            type="text"
+                                            value={lastName}
+                                            onChange={(e) => setLastName(e.target.value)}
+                                        />
+                                    </div>
+                                    <div className="grid gap-2">
+                                        <div className="flex items-center">
+                                            <Label htmlFor="password">Password</Label>
+                                        </div>
+                                        <Input id="password" type="password"
+                                               value={password}
+                                               onChange={(e) => setPassword(e.target.value)}
+                                               required />
+                                    </div>
+                                    <Button type="submit" className="w-full">
+                                        Sign up
+                                    </Button>
+                                    {/*<Button variant="outline" className="w-full">*/}
+                                    {/*    Login with Google*/}
+                                    {/*</Button>*/}
+                                </div>
+                                <div className="mt-4 text-center text-sm">
+                                    Already have an account?{" "}
+                                    <Link to="/login" className="underline underline-offset-4">
+                                        Login
+                                    </Link>
+                                </div>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 }
